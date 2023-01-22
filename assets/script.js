aaeab7244740795632e8d62a1f84a7071ebcc206
Liam <liamjwatson@aol.com>
2023-01-22 00:34:18 +0000

    row for each hour

--- assets/script.js
+++ assets/script.js
@@ -4,12 +4,20 @@ $("#currentDay").text(today.format("dddd, Do MMMM"));
 
 var currentHour = today.format("ha");
 
+currentHour = "12pm";
+
 console.log(currentHour);
 
-workArray = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+workArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 
+// For each hour create a div, with a class of the hour and text of the hour
 $(workArray).each(function (index) {
-  var hourDiv = $("<div>");
-
-  $(".container").append(hourDiv.addClass([index]));
+  var hourRow = $("<div>").addClass("row " + workArray[index]);
+  $(".container").append(
+    hourRow
+      .addClass([index])
+      .append(
+        "<div class='col-2'></div><textarea class='col-8'></textarea><button class='col-2 saveBtn'></button>"
+      )
+  );
 });
